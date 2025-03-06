6b3a1d9e0b13f97ed486ba2ee0a3d9adb86eee58
jin123457 <jin_ox@naver.com>
2025-03-07 00:47:02 +0900

    ✅ 3월 7일 Stack 문제 해결

---
+++ 2025.03/03.07/add_two_numbers.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    const pairParentheses = {
+        ')': '(',
+        '}': '{',
+        ']': '[',
+    };
+    const answer = [];
+    s.split('').forEach((parentheses) => {
+        if (pairParentheses[parentheses] === answer[answer.length - 1] && answer.length > 0) return answer.pop();
+
+        return answer.push(parentheses);
+    });
+
+    return answer.length > 0 ? false : true;
+};
+
+isValid('()');
