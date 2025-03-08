51898e7b49caf7c2db172849730fe442ca69c18e
kimyou1102 <yoosk5485@naver.com>
2025-03-09 00:57:39 +0900

    feat: 03/09 백준 DP 문제 해결

---
+++ 2025.03_1주차/03.09/11726.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().trim().split('\n')[0];
+
+const n = Number(input);
+
+const dp = Array(n + 1).fill(0);
+
+dp[1] = 1;
+dp[2] = 2;
+
+for (let i = 3; i <= n; i++) {
+  dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+}
+
+console.log(dp[n]);
