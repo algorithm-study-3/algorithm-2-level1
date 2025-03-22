f4a92f61fe3cb0f6b510f20d92b1f96efaab64e7
jin123457 <jin_ox@naver.com>
2025-03-22 22:55:42 +0900

    ⁉️ 3월 22일 Hash 문제 해결

---
+++ 2025.03/3주차/03.22/1번 문제/42577.js
@@ -0,0 +1,12 @@
+function solution(phone_book) {
+    let answer = true;
+    phone_book.sort();
+
+    for (let i = 1; i < phone_book.length; i++) {
+        if (phone_book[i].startsWith(phone_book[i - 1])) answer = false;
+    }
+
+    return answer;
+}
+
+console.log(solution(['119', '97674223', '1195524421']));
