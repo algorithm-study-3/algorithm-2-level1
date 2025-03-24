da6ff9e5fbd49a07a26a1e7d43dc474987f9b6dc
jin123457 <jin_ox@naver.com>
2025-03-24 14:44:42 +0900

    ✅ 3월 24일 String 문제 해결

---
+++ 2025.03/3주차/03.24/1번 문제/120956.js
@@ -0,0 +1,16 @@
+function solution(babbling) {
+    let answer = 0;
+    const babyWord = ['aya', 'ye', 'woo', 'ma'];
+
+    for (let x of babbling) {
+        babyWord.forEach((word) => {
+            x = x.replace(word, 'z');
+        });
+
+        if (x.replaceAll('z', '').length === 0) answer += 1;
+    }
+
+    return answer;
+}
+
+console.log(solution(['aya', 'yee', 'u', 'maa', 'wyeoo']));
