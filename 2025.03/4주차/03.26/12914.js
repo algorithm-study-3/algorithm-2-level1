1afebb4feea58fa7baf3f5caad8cea21a4d11150
jin123457 <jin_ox@naver.com>
2025-03-26 22:09:24 +0900

    ✅ 3월 26일 DP 문제 해결

---
+++ 2025.03/4주차/03.26/12914.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    const answer = [1, 2];
+    for (let i = 2; i <= n; i++) {
+        answer.push((answer[i - 1] + answer[i - 2]) % 1234567);
+    }
+
+    return answer[answer.length - 2];
+}
+
+solution(100);
